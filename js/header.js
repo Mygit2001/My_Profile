e97e9e3eba7bab607293e91995b65ee78293961d
Syle2001ht <77475693+Mygit2001@users.noreply.github.com>
2022-09-16 21:36:39 +0700

    upload code profile next1

--- js/header.js
+++ js/header.js
@@ -1,30 +1,45 @@
-  var header  = document.getElementById('header');
-        var mobileMenu = document.getElementById('mobile-menu');
-        var headerHeight = header.clientHeight;
+var header = document.getElementById("header");
+var btnMobileMenu = document.getElementById("mobile-menu");
+var headerHeight = header.clientHeight;
+var modalMenu = document.getElementById("modal");
+var menuHeaderMobile = document.getElementsByClassName("modal-container");
 
-        //đóng mở header mobile menu
-        mobileMenu.onclick= () => {
-           var isClosed= header.clientHeight === headerHeight;
-           if(isClosed){
-               header.style.height= 'auto';
-           }else{
-               header.style.height=null;
-           }
-        }
-        //đóng mở menu khi click vào item menu
-        var menuItems = document.querySelectorAll('#nav-menu-list li a');
-        for(var i=0 ;i<menuItems.length;i++){
-            var menuItem = menuItems[i];
-            menuItem.onclick = () => {
-                header.style.height=null;
-            }
-        }
-        var listLineActives = document.querySelectorAll('.add-active');
-        
-        listLineActives.forEach((listLineActive) => {
-            listLineActive.onclick = () => {
-                console.log(listLineActive);
-                document.querySelector('.add-active.line-bt-navItem').classList.remove('line-bt-navItem');
-                this.classList.add('line-bt-navItem');
-                }
-        })
+//  đóng mở header mobile menu
+
+btnMobileMenu.onclick = () => {
+  var isClosed = header.clientHeight === headerHeight;
+  
+  if (isClosed) {
+    header.style.height = "auto";
+  } else {
+    header.style.height = null;
+  }
+};
+
+//đóng mở menu khi click vào item menu
+var menuItems = document.querySelectorAll(".modal-menu-mobile li a");
+menuItems.forEach((menuItem) => {
+    menuItem.onclick = () => {
+        console.log("hi")
+        modalMenu.style.display = "none";
+        menuHeaderMobile.style.display = "none";
+    }
+})
+
+
+
+
+
+
+
+var listLineActives = document.querySelectorAll(".add-active");
+
+listLineActives.forEach((listLineActive) => {
+  listLineActive.onclick = () => {
+    console.log(listLineActive);
+    document
+      .querySelector(".add-active.line-bt-navItem")
+      .classList.remove("line-bt-navItem");
+    this.classList.add("line-bt-navItem");
+  };
+});
--- js/home.js
+++ js/home.js
@@ -1,29 +1,37 @@
 // home
-var butnMenu = document.getElementById("menu-btn-js");
+var buttMenu = document.getElementById("menu-btn-js");
 var modalMenu = document.getElementById("modal");
 var closeMenu = document.getElementById("close-btn-js");
 var headerItem = document.getElementById("header");
 var navMenuItem = document.getElementsByClassName("nav-menu");
 var backBtn = document.getElementById("back-to-top");
 
-butnMenu.onclick = () => {
+const closeModal = () => {
+  modalMenu.style.display = "none";
+  buttMenu.style.display = "block";
+  closeMenu.style.display = "none";
+}
+
+buttMenu.onclick = () => {
   modalMenu.style.display = "block";
-  butnMenu.style.display = "none";
+  buttMenu.style.display = "none";
   closeMenu.style.display = "block";
 };
 
 closeMenu.onclick = () => {
-  modalMenu.style.display = "none";
-  butnMenu.style.display = "block";
-  closeMenu.style.display = "none";
+  closeModal();
 };
 
+modalMenu.onclick = () => {
+  closeModal();
+}
 document.onscroll = () => {
   if (window.scrollY >= 560) {
     headerItem.style.display = "block";
     backBtn.style.display = "block";
     navMenuItem[0].style.display = "none";
     headerItem.classList.add("header-sm");
+    // buttMenu.style.display = "block";
   } else {
     headerItem.style.display = "none";
     backBtn.style.display = "none";
